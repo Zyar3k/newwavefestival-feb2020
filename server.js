1469b586386c29765bf748a02a5d895bf6b236ba
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-12 20:18:39 +0100

    Prepare for heroku deploy

--- server.js
+++ server.js
@@ -11,17 +11,24 @@ const app = express();
 app.use(
   cors({ origin: 'http://localhost:3000', methods: 'GET, POST, PUT, DELETE' })
 );
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use(express.json());
-app.use(cors());
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', concertsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send({message: '404 not found...'});
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
