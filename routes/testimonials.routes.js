2a316d35a866d5cf2de67643bcd0518ac4671a3e
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-13 21:20:39 +0100

    Add controllers and modify routes

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,45 +1,12 @@
 const express = require('express');
-const db = require('../db');
 const router = express.Router();
-const uuidv1 = require('uuid/v1');
 
-router.route('/testimonials').get((req, res) => {
-  res.json(db.testimonials);
-});
+const TestimonialController = require('../controllers/testimonials.controller');
 
-router.route('/testimonials/:id').get((req, res) => {
-  for(let post of db.testimonials){
-    if(post.id == req.params.id){
-      res.json(post);
-    };
-  };
-});
-
-router.route('/testimonials').post((req, res) => {
-  const {author, text} = req.body;
-  const newPost = {author: author, text: text, id: uuidv1()};
-  db.testimonials.push(newPost);
-  res.json({message: 'OK'});
-})
-
-router.route('/testimonials/:id').put((req, res) => {
-  const {author, text} = req.body;
-  for(let post of db.testimonials){
-    if(post.id == req.params.id){
-      post.author = author;
-      post.text = text;
-    };
-  };
-  res.json({message: 'OK'});
-})
-
-router.route('/testimonials/:id').delete((req, res) => {
-  for(let post of db.testimonials){
-    if(post.id == req.params.id){
-      db.testimonials.splice(db.testimonials.indexOf(post));
-    };
-  };
-  res.json({message: 'OK'});
-})
+router.get('/testimonials', TestimonialController.getAll);
+router.get('/testimonials/:id', TestimonialController.getOne);
+router.post('/testimonials', TestimonialController.postAll);
+router.put('/testimonials/:id', TestimonialController.updateOne);
+router.delete('/testimonials/:id', TestimonialController.deleteOne);
 
 module.exports = router;
